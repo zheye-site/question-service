467c72af382c5ae254552a213c0a6b7ce8ea317c
ysdeng <ysdeng1006@gmail.com>
2022-03-11 09:57:25 +0800

    Add database test configuration to the smoke test.

--- src/test/java/com/zheye/question/SmokeTest.java
+++ src/test/java/com/zheye/question/SmokeTest.java
@@ -1,9 +1,11 @@
 package com.zheye.question;
 
+import com.zheye.question.core.DatabaseTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(DatabaseTestConfiguration.class)
 public class SmokeTest {
 
     @Autowired
